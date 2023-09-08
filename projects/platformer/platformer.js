aee0abad7f9d4da442080fe32bff6d255a76d7a7
P0rpleStar M <andreamiles@mac.com>
2023-09-08 12:48:13 +0000

    platformer

--- projects/platformer/platformer.js
+++ projects/platformer/platformer.js
@@ -103,11 +103,11 @@ $(function () {
     // Your wall choices are: 'top' 'left' 'right' and 'bottom'
     // example usage: createCannon(side, position, delay, width, height)
 
-    createCannon("bottom", 900, 2000, 25, 25)
-    createCannon("top", 1120, 2000, 25, 25)
-    createCannon("bottom", 1100, 2000, 25, 25)
-    createCannon("top", 1320, 2000, 25, 25)
-    createCannon("bottom", 1300, 2000, 25, 25)
+    createCannon("bottom", 900, 2250, 25, 25)
+    createCannon("top", 1120, 2250, 25, 25)
+    createCannon("bottom", 1100, 2250, 25, 25)
+    createCannon("top", 1320, 2250, 25, 25)
+    createCannon("bottom", 1300, 2250, 25, 25)
 
     ////////////////////////////////////////////////
     //////////ONLY CHANGE ABOVE THIS POINT///////////
